42bfcd40f9e3d9318e36d45b879231b7f6217648
sethjpete <sethjpete@gmail.com>
2023-07-18 21:29:10 -0600

    Finsihed through part 6 of tutorial:
    https://dev.to/paurakhsharma/flask-rest-api-part-5-password-reset-2f2e

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_mail import Mail
 
 from database.db import initialize_db
 from flask_restful import Api
-from resources.routes import initialize_routes
+from resources.errors import errors
 
 app = Flask(__name__)
 app.config.from_envvar('ENV_FILE_LOCATION')
+mail = Mail(app)
 
-api = Api(app)
+from resources.routes import initialize_routes
+
+api = Api(app, errors=errors)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
@@ -19,5 +23,3 @@ app.config['MONGODB_SETTINGS'] = {
 
 initialize_db(app)
 initialize_routes(api)
-
-app.run()
