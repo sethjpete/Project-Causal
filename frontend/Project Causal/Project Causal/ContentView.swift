8c8824752c61f78a7f77f0db16307b33658819f2
sethjpete <sethjpete@gmail.com>
2023-07-20 15:27:49 -0600

    Initial Swift commit

---
+++ frontend/Project Causal/Project Causal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Project Causal
+//
+//  Created by Seth Peterson on 7/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ frontend/Project Causal/Project Causal/Project_CausalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_CausalApp.swift
+//  Project Causal
+//
+//  Created by Seth Peterson on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Project_CausalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
